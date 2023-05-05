6f758863c87c36119b1aab988416bfbbc6acddd6
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-05-05 19:51:10 +0300

    feature: expand progress block

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -5,10 +5,19 @@ import intersect from './intersect.svg';
 import logo from './logo.svg';
 import mainImage from './main.jpg';
 import newWayImage from './newWayImage.png';
+import progressImage from './progressImage.png';
 import riddec from './riddec.svg';
-export { mainImage, intersect, androidIcon, iosIcon, riddec };
-
-export { logo, newWayImage, crossImage };
+export {
+  mainImage,
+  intersect,
+  androidIcon,
+  iosIcon,
+  riddec,
+  logo,
+  newWayImage,
+  crossImage,
+  progressImage,
+};
 
 export { ReactComponent as HamsterPunkText } from './hamsterpunkText.svg';
 export { ReactComponent as RiddecCreated } from './riddec.svg';
