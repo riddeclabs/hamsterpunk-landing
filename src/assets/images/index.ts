59a58b470952a09eab5319ec5d1fefcfb5f5cbba
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-05-05 19:57:00 +0300

    feature: humsterville block

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,14 +1,23 @@
 import androidIcon from './android.svg';
 import iosIcon from './apple.svg';
 import crossImage from './crossImage.svg';
+import humstervilleImage from './humstervillImage.png';
 import intersect from './intersect.svg';
 import logo from './logo.svg';
 import mainImage from './main.jpg';
 import newWayImage from './newWayImage.png';
 import riddec from './riddec.svg';
-export { mainImage, intersect, androidIcon, iosIcon, riddec };
-
-export { logo, newWayImage, crossImage };
+export {
+  mainImage,
+  intersect,
+  androidIcon,
+  iosIcon,
+  riddec,
+  logo,
+  newWayImage,
+  crossImage,
+  humstervilleImage,
+};
 
 export { ReactComponent as HamsterPunkText } from './hamsterpunkText.svg';
 export { ReactComponent as RiddecCreated } from './riddec.svg';
--- src/pages/containers/HumstervilleBlock/index.tsx
+++ src/pages/containers/HumstervilleBlock/index.tsx
@@ -1,5 +1,54 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
+import { humstervilleImage } from '../../../assets/images';
+import { Content } from '../../../components/Content';
+import { Typography } from '../../../components/Typography';
+import { colors } from '../../../constants/variables';
+
 export const HumstervilleBlock = () => {
-  return <div>HumstervilleBlock</div>;
+  //TODO change color and BG
+  return (
+    <Block>
+      <Content>
+        <Wrapper>
+          <Typography
+            text='Hamsterville'
+            variant='title'
+            color={colors.mainText}
+          />
+          <Text>
+            <Typography
+              color={colors.mainText}
+              text='Hamsterville is a small island, floating in the sky. Lead the exploration of new lands and expand the island to give your hamsters more space to live in.
+'
+              variant='text'
+            />
+          </Text>
+
+          <ProgressImage alt='progress' src={humstervilleImage} />
+        </Wrapper>
+      </Content>
+    </Block>
+  );
 };
+
+const Block = styled.div(() => ({
+  marginTop: '180px',
+}));
+
+const Wrapper = styled.div(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column' || theme.column,
+  alignItems: 'center',
+  textAlign: 'center',
+}));
+
+const ProgressImage = styled.img(() => ({
+  marginTop: '52px',
+}));
+const Text = styled.div(() => ({
+  marginTop: '24px',
+  maxWidth: '598px',
+}));
