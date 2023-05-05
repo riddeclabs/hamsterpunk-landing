6d6b6eec0b8e0aca8ad199bc749b9ecfd1625095
mr-teddy-bear <sokolovskiy-11111@mail.ru>
2023-05-05 11:27:40 +0300

    fix: add pages

---
+++ src/components/TestButton/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const TestButtonComponent = () => {
+  return <div>AdvantagesBlock</div>;
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,34 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+import { AdvantagesBlock } from './containers/AdvantagesBlock';
+import { FAQBlock } from './containers/FAQBlock';
+import { Footer } from './containers/Footer';
+import { GalleryBlock } from './containers/GalleryBlock';
+import { HumstervilleBlock } from './containers/HumstervilleBlock';
+import { MainBlock } from './containers/MainBlock';
+import { Menu } from './containers/Menu';
+import { NewWayBlock } from './containers/NewWayBlock';
+import { ProgressBlock } from './containers/ProgressBlock';
+import { VideoBlock } from './containers/VideoBlock';
+
 import { colors } from '../constants/variables';
 
 export const HomePage = () => {
-  return <Wrapper>Test</Wrapper>;
+  return (
+    <Wrapper>
+      <Menu />
+      <MainBlock />
+      <NewWayBlock />
+      <AdvantagesBlock />
+      <VideoBlock />
+      <GalleryBlock />
+      <ProgressBlock />
+      <HumstervilleBlock />
+      <FAQBlock />
+      <Footer />
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div(() => ({
